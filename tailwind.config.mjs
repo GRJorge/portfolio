59cd6b549d489589a226116eb0dc8b1cdadc2543
grjorge <deb.devel@outlook.es>
2024-12-06 00:25:18 -0600

    Theme colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      dark: {
+        100: "#504945",
+        200: "#3C3836",
+        300: "#282828",
+        400: "1D2021",
+      },
+      light: "#EBDBB2",
+      primary: "#8EC07C",
+      secondary: "D3869B",
+      green: "B8BB26",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
