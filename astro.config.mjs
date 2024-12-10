86c2cff274f94c54687d085eef4341152c01075e
grjorge <deb.devel@outlook.es>
2024-12-10 02:37:32 -0600

    Configuration to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: "https://grjorge.github.io",
+  base: "portfolio"
 });
